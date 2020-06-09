347d7cda4e7cdf432d8e56e13919d8a65b46eb9a
Chandu Reddy <aslan.@7>
2020-06-09 16:04:32 +0530

    Initial Commit

---
+++ PracticeApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticeApplication
+//
+//  Created by Chandu Reddy on 11/05/20.
+//  Copyright © 2020 Chandu Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func emailField(_ sender: Any) {
+        
+    }
+    
+    
+    
+}
+
